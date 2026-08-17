5ecd304e0b2386eaf58e66dc23e7f25c46f8ff7f
tosoun <kostaszp4@gmail.com>
2026-08-17 11:24:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import streamlit.components.v1 as components
 import base64
+import glob
 
 st.set_page_config(page_title="Πωλήσεις ανά Κατάστημα", layout="centered")
 
@@ -20,7 +21,6 @@ excel_path = "tv sat sales.xlsx"
 time_path = "upload_time.txt"
 confetti_path = "confetti_status.txt"
 cheer_path = "cheer_status.txt"
-banner_filename = "ChatGPT Image 17 Αυγ 2026, 11_15_22 π.μ._2.png"
 
 confetti_enabled = True
 if os.path.exists(confetti_path):
@@ -179,7 +179,9 @@ try:
         max_sales = 1
 
     img_src = ""
-    if os.path.exists(banner_filename):
+    banner_files = glob.glob("ChatGPT Image*.png") + glob.glob("*banner*.jpg") + glob.glob("*banner*.png")
+    if banner_files:
+        banner_filename = banner_files[0]
         with open(banner_filename, "rb") as image_file:
             img_src = f"data:image/png;base64,{base64.b64encode(image_file.read()).decode()}"
 
