de09266aba82bcc5f87a8f3d0286d163580c1c23
tosoun <kostaszp4@gmail.com>
2026-08-17 11:40:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -213,7 +213,7 @@ try:
         overflow: hidden;
     }}
     
-    .banner-img {{ width: 100%; height: 130px; object-fit: cover; display: block; border-radius: 0; margin: 0; padding: 0; }}
+    .banner-img {{ width: 100%; height: 110px; object-fit: contain; background-color: #000; display: block; border-radius: 0; margin: 0; padding: 0; }}
     .content-wrapper {{ padding: 25px; }}
     
     .top-left-area {{
